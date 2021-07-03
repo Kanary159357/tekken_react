be6f028f21dd0636410367ac5400283323757bcc
kanary <kanary159357@gmail.com>
2021-07-03 22:50:40 +0900

    send Modal function

--- src/Context/ModalContext.tsx
+++ src/Context/ModalContext.tsx
@@ -1,27 +1,41 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, {
+    createContext,
+    Dispatch,
+    useContext,
+    useEffect,
+    useReducer,
+    useState,
+} from 'react';
 
 interface ModalProps {
+    modalAction: any;
     modalOpen: boolean;
-    setModal: any;
 }
+type Action = { type: 'SET'; payload: any } | { type: 'ADD'; payload: any };
 
+type ModalDispatch = Dispatch<Action>;
 const ModalContext = createContext<ModalProps | null>(null);
+const ModalDispatchContext = createContext<ModalDispatch | null>(null);
+
+const initialState = {
+    modalAction: 'add',
+    modalOpen: false,
+};
 
 export function ModalProvider({ children }: { children: React.ReactNode }) {
-    const [modalOpen, setModal] = useState(true);
-
-    useEffect(() => {
-        console.log(modalOpen);
-    }, [modalOpen]);
     return (
-        <ModalContext.Provider value={{ modalOpen, setModal }}>
-            {children}
-        </ModalContext.Provider>
+        <ModalContext.Provider value={state}>{children}</ModalContext.Provider>
     );
 }
 
 export function useModalData() {
     const data = useContext(ModalContext);
+    if (!data) throw new Error('Cannot find Provider');
+    return data;
+}
+
+export function useModalDispatch() {
+    const data = useContext(ModalDispatchContext);
     if (!data) throw new Error('Cannot find Provider');
     return data;
 }
